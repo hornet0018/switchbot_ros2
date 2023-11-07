3fb8e81ebd568cd442321bb99ba39463e759973f
kenta-uematsu <kenta_722@yahoo.co.jp>
2023-11-07 21:13:47 +0900

    launchファイル追加

---
+++ launch/multi_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='udco2s_ros2',
+            executable='get_data',
+            name='udco2s_node'
+        ),
+        Node(
+            package='switchbot_ros2',
+            executable='get_Mater',
+            name='switchbot_node'
+        ),
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            arguments=['udp4', '--port', '8888'],
+            name='micro_ros_agent_node'
+        )
+    ])
--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import find_packages, setup
+import os
 
 package_name = 'switchbot_ros2'
+
+# launchディレクトリ内のファイルのリストを取得する関数
+def get_launch_files():
+    launch_dir = os.path.join('launch')
+    launch_files = []
+    # launchディレクトリが存在するか確認
+    if os.path.exists(launch_dir):
+        # ディレクトリ内のファイルのみをリストアップ（ディレクトリとpycファイルは除外）
+        for filename in os.listdir(launch_dir):
+            filepath = os.path.join(launch_dir, filename)
+            if os.path.isfile(filepath) and not filename.endswith('.pyc'):
+                launch_files.append(filepath)
+    return launch_files
 
 setup(
     name=package_name,
@@ -10,6 +24,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', get_launch_files()),  # launchファイルを追加
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +35,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'get_Mater = switchbot_ros2.get_Mater:main'
+            'get_Mater = switchbot_ros2.get_Mater:main',
         ],
     },
 )
